fdadb8b2128144fb58b37a83a0c3e030164c5857
Tom Lawrence <2392780+tomlawrence@users.noreply.github.com>
2024-12-07 17:34:55 +0000

    Day 6 complete (re-rewritten implementation after spending hours trying to solve part 2 until I realised I had the correct answer all along and I had accidentally made a typo when inputting the answer on the AoC website D:)

--- day06/day06.go
+++ day06/day06.go
@@ -47,8 +47,9 @@ func main() {
 		return
 	}
 
-	var part1Count int = getVisitedPositionsCount(mapGrid, startPos)
-	var part2Count int = getObstacleInfiniteLoopCount(mapGrid, startPos)
+  var visitedPos, _ = getVisitedPositions(mapGrid, startPos)
+	var part1Count int = len(visitedPos)
+	var part2Count int = getObstacleInfiniteLoopCount(mapGrid, startPos, visitedPos)
 	fmt.Println("Part 1 - Number of locations visited by guard:", part1Count)
 	fmt.Println("Part 2 - Number of infinite loop obstacle locations:", part2Count)
 }
@@ -62,34 +63,22 @@ func newGrid(mapGrid [][]byte) [][]byte {
 	return newGrid
 }
 
-func getObstacleInfiniteLoopCount(mapGrid [][]byte, startPos Position) int {
+func getObstacleInfiniteLoopCount(mapGrid [][]byte, startPos Position, visitedPos map[Position]bool) int {
 	var obstacleCount int
-
-	for y, row := range mapGrid {
-		for x, col := range row {
-			if col == '^' || col == '>' || col == 'V' || col == '<' || col == '#' {
-			} else {
-				testGrid := newGrid(mapGrid)
-				testGrid[y][x] = '#'
-				if getVisitedPositionsCount(testGrid, startPos) == 30000 {
-					obstacleCount++
-				}
-			}
-			// if col != '#' || col != '^' || col != '>' || col != 'V' || col != '<' {
-			// 	testGrid := newGrid(mapGrid)
-			// 	testGrid[y][x] = '#'
-			// 	if getVisitedPositionsCount(testGrid, startPos) == 25000 {
-			//        fmt.Println("Obstacle at ", y, x)
-			// 		obstacleCount++
-			// 	}
-			// }
-		}
-	}
-
+  // Remove the starting position from the list of visited places we can test an obstacle at
+  delete(visitedPos, Position{startPos.y, startPos.x})
+  for pos := range visitedPos{
+    testGrid := newGrid(mapGrid)
+    testGrid[pos.y][pos.x] = '#'
+    var _, isALoop = getVisitedPositions(testGrid, startPos)
+    if isALoop {
+      obstacleCount++
+    }
+  }
 	return obstacleCount
 }
 
-func getVisitedPositionsCount(mapGrid [][]byte, currentPos Position) int {
+func getVisitedPositions(mapGrid [][]byte, currentPos Position) (map[Position]bool, bool) {
 	directions := map[byte]Position{
 		'^': {-1, 0},
 		'>': {0, 1},
@@ -97,23 +86,24 @@ func getVisitedPositionsCount(mapGrid [][]byte, currentPos Position) int {
 		'<': {0, -1},
 	}
 	turnOrder := []byte{'^', '>', 'V', '<'}
-	stepCount := 0
-
 	currentDir := mapGrid[currentPos.y][currentPos.x]
 	visitedPos := make(map[Position]bool)
+	obstaclesHit := make(map[Position]byte)
+  isALoop := false
 
 	for {
-		stepCount++
-		if stepCount == 30000 {
-			return stepCount
-		}
 		visitedPos[currentPos] = true
 		nextPos := Position{currentPos.y + directions[currentDir].y, currentPos.x + directions[currentDir].x}
 		if nextPos.y < 0 || nextPos.y >= len(mapGrid) || nextPos.x < 0 || nextPos.x >= len(mapGrid[0]) {
 			break
 		}
-		// var confirmedClear bool
 		if mapGrid[nextPos.y][nextPos.x] == '#' {
+      if obstaclesHit[nextPos] == currentDir {
+        isALoop = true
+        return visitedPos, isALoop
+      } else {
+        obstaclesHit[nextPos] = currentDir
+      }
 			for i, dir := range turnOrder {
 				if dir == currentDir {
 					currentDir = turnOrder[(i+1)%4]
@@ -124,17 +114,26 @@ func getVisitedPositionsCount(mapGrid [][]byte, currentPos Position) int {
 		}
 		currentPos = nextPos
 	}
-	return len(visitedPos)
+	return visitedPos, isALoop
 }
 
-/* Map ^>V< to direction vectors
-create map of visited coords
-read map from txt file
-find starting point (contains ^>V<)
-while not out of bounds
-mark current location as visited
-add to count
-check in direction ahead for obstacle "#"
-if not, move forward to next location
-else change direction to next in map
-*/
+// Part 1 Notes
+// ------------------------
+// Map ^>V< to direction vectors
+// Create map of visited coords
+// Read map from txt file
+// Find starting point (contains ^>V<)
+// While not out of bounds
+// Mark current location as visited
+// Add to count
+// Check in direction ahead for obstacle "#"
+// If not, move forward to next location
+// Else change direction to next in map
+
+// Part 2 Notes
+// ------------------------
+// Only need to check locations where you initially walked
+// Store location and direction of encounter with each obstacle
+// Generate and test a new mapGrid with the new obstacle in place
+// If you ever encounter an obstacle from the same direction, you must be stuck in a loop
+// Therefore increment the loop counter and test the next location
